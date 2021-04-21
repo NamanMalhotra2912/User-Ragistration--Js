ddabf3fa2b52901437cfa7cce6281d5f7b8bdc47
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-04-21 22:09:15 +0530

    Validate First name

--- User_Ragistration.js
+++ User_Ragistration.js
@@ -1,27 +1,18 @@
+const readline = require('readline-sync');
+
 console.log("Welcome to User Ragistration Program");
 
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function validateUser (){
 
-class UserRagistration{
-
-    firstName() {
-        readline.question('Please enter your first name\t', name =>{
-            let regex = /^[A-Z][a-zA-Z]{2}/
-            let verify = regex.test(name);
-        
-            if(verify == true){
-                console.log("First Name is : "+name);
-            }
-            else{
-                console.log("!! Error !! \nFirst name should contain first capital letter with menimum lenth of 3 characters");
-            }
-            readline.close();
-        })
+    // UC1 First Name validation
+    let firstName = readline.question("Please enter first name : ");
+    let regexFirstName = /^[A-Z][a-zA-Z]{2}/;
+    if (regexFirstName.test(firstName) == true){
+        console.log("First name is : ",firstName);
+    }
+    else{
+        console.log("!! Error !! \nFirst name should contain first capital letter with menimum lenth of 3 characters");
     }
 }
 
-const object = new UserRagistration();
-object.firstName();
+validateUser();
